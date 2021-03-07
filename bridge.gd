91d17cd2632e61646cd18ee36741ef8759115964
pondus314 <peter.ondus@gmail.com>
2021-03-07 01:29:11 +0100

    make bridges functional, add collision layers

---
+++ bridge.gd
@@ -0,0 +1,30 @@
+extends Area2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	connect("area_entered", self, "on_area_enter")
+	connect("area_exited", self, "on_area_exit")
+	pass # Replace with function body.
+
+const TILEMAP = 0
+
+func on_area_enter(area):
+	print("baa")
+	print(area.get_parent().tags_list)
+	area.get_parent().set_collision_mask_bit(TILEMAP, false)
+
+
+func on_area_exit(area):
+	area.get_parent().set_collision_mask_bit(TILEMAP, true)
+	area.get_parent().set_collision_layer_bit(TILEMAP, false)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
